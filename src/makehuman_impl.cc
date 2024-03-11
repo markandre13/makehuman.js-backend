c1f517f114ad2ff6d2724b9fa216a004edd9edc6
Mark-André Hopf <mhopf@mark13.org>
2024-03-11 18:06:17 +0100

    use idl file from makehuman.js repository

--- src/makehuman_impl.cc
+++ src/makehuman_impl.cc
@@ -5,5 +5,6 @@ Backend_impl::Backend_impl(std::shared_ptr<CORBA::ORB> orb) : Backend_skel(orb) 
 
 CORBA::async<> Backend_impl::setFrontend(std::shared_ptr<Frontend> aFrontend) {
     frontend = aFrontend;
+    co_await frontend->hello();
     co_return;
 }
