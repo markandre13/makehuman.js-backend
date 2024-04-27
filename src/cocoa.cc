7646341f7131023949078ffa8f40de0e7a34a3f3
Mark-André Hopf <mhopf@mark13.org>
2024-04-27 13:03:51 +0200

    cocoa: fill window with metal and set focus on app window on start

--- src/cocoa.cc
+++ src/cocoa.cc
@@ -1,4 +1,5 @@
 #import <Cocoa/Cocoa.h>
+#import <MetalKit/MetalKit.h>
 
 #include <print>
 
@@ -21,28 +22,79 @@ using namespace std;
 
 /////////////////////////////////////////////
 
+@interface Renderer : NSObject <MTKViewDelegate> {
+    MTKView *view;
+    id<MTLDevice> device;
+    id<MTLCommandQueue> commandQueue;
+}
+@end
+
+@implementation Renderer : NSObject
+- (nonnull instancetype)initWithMetalKitView:(nonnull MTKView *)aView {
+    if (self = [super init]) {
+        // Initialize self
+        view = aView;
+        commandQueue = [view.device newCommandQueue];
+    }
+    return self;
+}
+- (void)mtkView:(nonnull MTKView *)view drawableSizeWillChange:(CGSize)size {
+    println("metal view size {}x{}", size.width, size.height);
+}
+- (void)drawInMTKView:(nonnull MTKView *)view {
+    println("metal view draw");
+    id pool = [NSAutoreleasePool new];
+
+    id<MTLCommandBuffer> cmd = [commandQueue commandBuffer];
+    // MTL::RenderPassDescriptor* pRpd = pView->currentRenderPassDescriptor();
+    // MTL::RenderCommandEncoder* pEnc = pCmd->renderCommandEncoder( pRpd );
+    // pEnc->endEncoding();
+    // pCmd->presentDrawable( pView->currentDrawable() );
+    [cmd presentDrawable:[self->view currentDrawable]];
+    // pCmd->commit();
+    [cmd commit];
+
+    [pool release];
+}
+
+@end
+
+/////////////////////////////////////////////
 @interface ToadDelegate : NSObject <NSApplicationDelegate> {
 }
+- (void)applicationWillFinishLaunching:(NSNotification *)notification;
+- (void)applicationDidFinishLaunching:(NSNotification *)notification;
 - (void)createWindow;
 - (void)createMenu;
-// - (void) applicationWillFinishLaunching:(NSNotification *)notification;
-- (void)applicationDidFinishLaunching:(NSNotification *)notification;
 // - (void) windowWillMove:(NSNotification *)notification;
 // - (void) windowDidMove:(NSNotification *)notification;
 @end
 
 @implementation ToadDelegate : NSObject
 
-- (void)createMenu {
-    println("create menu...");
+- (void)applicationWillFinishLaunching:(NSNotification *)notification;
+{
+    println("applicationWillFinishLaunching...");
     // normal app with menu and menu item
     [NSApp setActivationPolicy:NSApplicationActivationPolicyRegular];
+    [self createMenu];
+}
+- (void)applicationDidFinishLaunching:(NSNotification *)notification;
+{
+    println("applicationDidFinishLaunching...");
+    [self createWindow];
+    [NSApp activateIgnoringOtherApps:YES];
+}
+- (BOOL)applicationShouldTerminateAfterLastWindowClosed:(NSApplication *)Sender {
+    return YES;
+}
 
+- (void)createMenu {
+    println("create menu...");
     id menubar = [[NSMenu new] autorelease];
     id appMenuItem = [[NSMenuItem new] autorelease];
     [menubar addItem:appMenuItem];
     [NSApp setMainMenu:menubar];
-
 
     id appMenu = [[NSMenu new] autorelease];
     id appName = [[NSProcessInfo processInfo] processName];
@@ -55,28 +107,41 @@ using namespace std;
 - (void)createWindow {
     println("create window...");
     // toadView *view = [[toadView alloc] initWithFrame:NSMakeRect(0, 0, 320, 200)];
-    id window = [[NSWindow alloc]
-        initWithContentRect:NSMakeRect(0, 0, 640, 480)
-                  styleMask:NSWindowStyleMaskTitled | NSWindowStyleMaskMiniaturizable | NSWindowStyleMaskClosable | NSWindowStyleMaskResizable
-                    backing:NSBackingStoreBuffered
-                      defer:NO];
-    [window cascadeTopLeftFromPoint:NSMakePoint(20,20)];
-    [window setBackgroundColor:[NSColor blueColor]];
+    id window =
+        [[NSWindow alloc] initWithContentRect:NSMakeRect(0, 0, 640, 480)
+                                    styleMask:NSWindowStyleMaskTitled | NSWindowStyleMaskMiniaturizable | NSWindowStyleMaskClosable | NSWindowStyleMaskResizable
+                                      backing:NSBackingStoreBuffered
+                                        defer:NO];
+    [window cascadeTopLeftFromPoint:NSMakePoint(20, 20)];
+    // [window setBackgroundColor:[NSColor blueColor]];
 
     id appName = [[NSProcessInfo processInfo] processName];
-    [window setTitle: appName];
+    [window setTitle:appName];
 
     [window makeKeyAndOrderFront:NSApp];
-}
 
-- (void)applicationWillFinishLaunching:(NSNotification *)notification;
-{
-    println("applicationWillFinishLaunching...");
-    [self createMenu];
-    [self createWindow];
-}
-- (BOOL)applicationShouldTerminateAfterLastWindowClosed:(NSApplication *)Sender {
-    return YES;
+    //
+    // METAL
+    //
+
+    id<MTLDevice> device;
+    device = MTLCreateSystemDefaultDevice();
+    if (!device) {
+        println("no metal default device\n");
+    } else {
+        println("metal default device: {}\n", [[device name] cStringUsingEncoding:NSISOLatin1StringEncoding]);
+    }
+
+    MTKView *view = [MTKView new];
+    view.device = device;
+    view.clearColor = MTLClearColorMake(0.0, 0.5, 1.0, 1.0);
+    view.enableSetNeedsDisplay = YES;
+
+    Renderer *renderer = [[Renderer alloc] initWithMetalKitView: view];
+    // Renderer *renderer = [Renderer new];
+    view.delegate = renderer;
+
+    [window setContentView:view];
 }
 
 @end
@@ -89,7 +154,7 @@ int main() {
 
     ToadDelegate *delegate = [ToadDelegate new];
     [app setDelegate:delegate];
-    [app activateIgnoringOtherApps:YES];
+    // [app activateIgnoringOtherApps:YES];
 
     [app run];
     [pool release];
