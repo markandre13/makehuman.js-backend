f494997c6ddedff00ddeab0b6c6897160099e97f
Mark-André Hopf <mhopf@mark13.org>
2024-04-28 09:57:39 +0200

    cocoa: refactor

--- src/metal/metal.cc
+++ src/metal/metal.cc
@@ -24,6 +24,9 @@ using namespace std;
     }
     return self;
 }
+// halfX : float16[X]
+// floatX: float32[X]
+
 - (void)buildShaders {
     println("build shaders");
 
@@ -31,40 +34,37 @@ using namespace std;
             #include <metal_stdlib>
             using namespace metal;
 
-            struct v2f
-            {
+            // vertex shader out
+            struct v2f {
                 float4 position [[position]];
                 half3 color;
             };
 
+            // we get two buffers from the app and a vertex number
             v2f vertex vertexMain( uint vertexId [[vertex_id]],
                                 device const float3* positions [[buffer(0)]],
-                                device const float3* colors [[buffer(1)]] )
-            {
+                                device const float3* colors [[buffer(1)]] ) {
                 v2f o;
                 o.position = float4( positions[ vertexId ], 1.0 );
                 o.color = half3 ( colors[ vertexId ] );
                 return o;
             }
 
-            half4 fragment fragmentMain( v2f in [[stage_in]] )
-            {
+            half4 fragment fragmentMain( v2f in [[stage_in]] ) {
                 return half4( in.color, 1.0 );
             }
         )";
 
+    // compile shaders
     NSError *error;
     id<MTLLibrary> pLibrary = [_device newLibraryWithSource:[NSString stringWithUTF8String:shaderSrc] options:nil error:&error];
     if (!pLibrary) {
         [NSException raise:@"Failed to compile shaders" format:@"%@", [error localizedDescription]];
     }
 
-    id<MTLFunction> pVertexFn = [pLibrary newFunctionWithName:@"vertexMain"];
-    id<MTLFunction> pFragFn = [pLibrary newFunctionWithName:@"fragmentMain"];
-
-    MTLRenderPipelineDescriptor *pDesc = [[MTLRenderPipelineDescriptor alloc] init];
-    pDesc.vertexFunction = pVertexFn;
-    pDesc.fragmentFunction = pFragFn;
+    MTLRenderPipelineDescriptor *pDesc = [MTLRenderPipelineDescriptor new];
+    pDesc.vertexFunction = [pLibrary newFunctionWithName:@"vertexMain"];
+    pDesc.fragmentFunction = [pLibrary newFunctionWithName:@"fragmentMain"];
     pDesc.colorAttachments[0].pixelFormat = MTLPixelFormatBGRA8Unorm_sRGB;
 
     _pPSO = [_device newRenderPipelineStateWithDescriptor:pDesc error:&error];
@@ -72,30 +72,27 @@ using namespace std;
         [NSException raise:@"Failed to create pipeline state" format:@"%@", [error localizedDescription]];
     }
 
-    [pVertexFn release];
-    [pFragFn release];
+    // [pVertexFn release];
+    // [pFragFn release];
     [pDesc release];
     [pLibrary release];
 }
 - (void)buildBuffers {
     println("build buffers");
 
-    const size_t NumVertices = 3;
+    const size_t nVertices = 3;
 
-    simd::float3 positions[NumVertices] = {{-0.8f, 0.8f, 0.0f}, {0.0f, -0.8f, 0.0f}, {+0.8f, 0.8f, 0.0f}};
-    simd::float3 colors[NumVertices] = {{1.0, 0.3f, 0.2f}, {0.8f, 1.0, 0.0f}, {0.8f, 0.0f, 1.0}};
+    simd::float3 positions[nVertices] = {{-0.8f, 0.8f, 0.0f}, {0.0f, -0.8f, 0.0f}, {+0.8f, 0.8f, 0.0f}};
+    simd::float3 colors[nVertices] = {{1.0, 0.3f, 0.2f}, {0.8f, 1.0, 0.0f}, {0.8f, 0.0f, 1.0}};
 
-    const size_t positionsDataSize = NumVertices * sizeof(simd::float3);
-    const size_t colorDataSize = NumVertices * sizeof(simd::float3);
+    const size_t nbytesPositions = nVertices * sizeof(simd::float3);
+    const size_t nbytesColors = nVertices * sizeof(simd::float3);
 
-    id<MTLBuffer> pVertexPositionsBuffer = [_device newBufferWithLength:positionsDataSize options:MTLResourceStorageModeManaged];
-    id<MTLBuffer> pVertexColorsBuffer = [_device newBufferWithLength:colorDataSize options:MTLResourceStorageModeManaged];
+    _pVertexPositionsBuffer = [_device newBufferWithLength:nbytesPositions options:MTLResourceStorageModeManaged];
+    _pVertexColorsBuffer = [_device newBufferWithLength:nbytesColors options:MTLResourceStorageModeManaged];
 
-    _pVertexPositionsBuffer = pVertexPositionsBuffer;
-    _pVertexColorsBuffer = pVertexColorsBuffer;
-
-    memcpy([_pVertexPositionsBuffer contents], positions, positionsDataSize );
-    memcpy([_pVertexColorsBuffer contents], colors, colorDataSize );
+    memcpy([_pVertexPositionsBuffer contents], positions, nbytesPositions );
+    memcpy([_pVertexColorsBuffer contents], colors, nbytesColors );
 
     [_pVertexPositionsBuffer didModifyRange: NSMakeRange( 0, [_pVertexPositionsBuffer length])];
     [_pVertexColorsBuffer didModifyRange: NSMakeRange( 0, [_pVertexColorsBuffer length])];
@@ -128,8 +125,6 @@ using namespace std;
 @end
 
 /////////////////////////////////////////////
-
-
 
 int main() {
     println("Metal...");
