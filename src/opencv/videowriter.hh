c2c70be1aa5425190a943d150e01901a7eec9de6
Mark-André Hopf <mhopf@mark13.org>
2024-07-28 15:17:03 +0200

    video: change codec from mjpg to h.264

--- src/opencv/videowriter.hh
+++ src/opencv/videowriter.hh
@@ -15,7 +15,14 @@ class VideoWriter {
 
         void frame(const cv::Mat &frame, double fps) {
             if (!isOpen) {
-                int codec = cv::VideoWriter::fourcc('M', 'J', 'P', 'G');
+                // int codec = cv::VideoWriter::fourcc('M', 'J', 'P', 'G');
+                // macos can't show this but opencv
+                // int codec = cv::VideoWriter::fourcc('H', '2', '6', '4');
+                // MPEG-4 Part 2
+                // int codec = cv::VideoWriter::fourcc('M', 'P', '4', 'V');
+                // MPEG-4 Part 10 aka. H.264
+                int codec = cv::VideoWriter::fourcc('a', 'v', 'c', '1');
+
                 bool isColor = true;
                 writer.open(filename, codec, fps, frame.size(), isColor);
                 if (!writer.isOpened()) {
